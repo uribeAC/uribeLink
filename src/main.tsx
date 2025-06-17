1482ca98a701f17d5799a88ddb779f9f7babace1
Alex Uribe <repzyyog@gmail.com>
2025-06-17 18:58:31 +0200

    Create Layout component with styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/styles.css";
+import App from "./ui/components/App/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className="main-container"></div>
+    <App />
   </StrictMode>,
 );
---
+++ src/ui/components/Layout/Layout.tsx
@@ -0,0 +1,9 @@
+import type React from "react";
+
+const Layout: React.FC = () => {
+  return (
+    <div className="container min-h-screen min-w-screen h-14 bg-linear-to-t from-sky-500 to-indigo-500"></div>
+  );
+};
+
+export default Layout;
