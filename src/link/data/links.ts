c1c586e9bc6da7256b1e6716e1f1e936d6ba1912
Alex Uribe <repzyyog@gmail.com>
2025-10-21 12:40:17 +0200

    Create links data

---
+++ src/link/data/links.ts
@@ -0,0 +1,31 @@
+import type { Link } from "../types";
+
+export const curriculumLink: Link = {
+  iconUrl: "/Read-Cv-Logo--Streamline-Phosphor.svg",
+  link: "/CV Alex Uribe.pdf",
+  text: "Check my CV",
+};
+
+export const emailLink: Link = {
+  iconUrl: "/Mail--Streamline-Tabler.svg",
+  link: "mailto:alex5uribe@gmail.com",
+  text: "Send me an email",
+};
+
+export const linkedinLink: Link = {
+  iconUrl: "/Linkedin--Streamline-Core-Remix.svg",
+  link: "https://www.linkedin.com/in/alex-uribe-crespo/",
+  text: "More about me",
+};
+
+export const githubLink: Link = {
+  iconUrl: "/Github--Streamline-Iconoir.svg",
+  link: "https://github.com/uribeAC",
+  text: "Explore my repos",
+};
+
+export const goodreadsLink: Link = {
+  iconUrl: "/Goodreads-Logo--Streamline-Phosphor.svg",
+  link: "https://www.goodreads.com/user/show/84064620-uribeac",
+  text: "Beyond the screen",
+};
